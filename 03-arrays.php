2ed417d088f35a86b8b628086d43330d4e25d936
alanssmeira <alan.sobreirasa@gmail.com>
2022-04-27 14:49:00 -0300

    Update 03-arrays.php

--- 03-arrays.php
+++ 03-arrays.php
@@ -45,5 +45,21 @@
         O curso de <?=$curso["nome"]?> tem <?=$curso["carga-horaria"]?> horas.
     </p>
 
+    <hr>
+
+    <h2>Funções de teste (debug)</h2>
+
+    <pre>
+    <?=print_r($cursos)?>
+    </pre>
+
+    <!-- mostra detalhes da arrays e objetos -->
+    <pre>
+        <?=var_dump($cursos)?>
+    </pre>
+
+    
+
+
 </body>
 </html>
