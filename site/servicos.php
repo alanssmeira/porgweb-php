10db6388ae93c818338519955230d4f385aeeb49
alanssmeira <alan.sobreirasa@gmail.com>
2022-05-04 14:50:46 -0300

    Create servicos.php

---
+++ site/servicos.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Serviços - ite XYZ</title>
+</head>
+<body>
+
+    <header>
+        <h1>Site XYZ</h1>
+
+        <nav>
+            <a href="index.php">Home</a>
+            <a href="produtos.php">Produtos</a>
+            <a href="servicos.php">Serviços</a>
+        </nav>
+    </header>
+
+    <main>
+        <h2>Página de Serviços</h2>
+        <hr>
+
+        <p>Nossos especialistas atuam em uma séie de serviços.</p>
+        <p>Veja a seguir e escloha o que precisa.</p>
+
+        <h3>Lista de Serviços</h3>
+        <ol>
+            <li>Instalação de redes</li>
+            <li>Manutenção de computadores</li>
+            <li>Configuração de programas</li>
+        </ol>
+    </main>
+
+    <footer>
+        <p>Site desenvolvido como exemplo</p>
+    </footer>
+    
+</body>
+</html>
