c73fe226eeeeaa0420c43976479d3ce8d6f7f1ad
alanssmeira <alan.sobreirasa@gmail.com>
2022-05-04 16:36:07 -0300

    Create contato.php

---
+++ site/contato.php
@@ -0,0 +1,11 @@
+<?php require "includes/cabecalho.php" ?>
+
+    <h2>Página de Contato</h2>
+    <hr>
+
+    <p>Dúvidas?</p>
+    <p>Entre em contato conosco!</p>
+
+    
+
+    <?php require "includes/rodape.php" ?>
