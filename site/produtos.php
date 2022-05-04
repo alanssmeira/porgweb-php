eaf1ccb5a0e4e4a07caa79641c861840f0f18c12
alanssmeira <alan.sobreirasa@gmail.com>
2022-05-04 16:36:32 -0300

    Update produtos.php

--- site/produtos.php
+++ site/produtos.php
@@ -1,24 +1,5 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Produtos - Site XYZ</title>
-</head>
-<body>
+<?php require "includes/cabecalho.php" ?>
 
-    <header>
-        <h1>Site XYZ</h1>
-
-        <nav>
-            <a href="index.php">Home</a>
-            <a href="produtos.php">Produtos</a>
-            <a href="servicos.php">Serviços</a>
-        </nav>
-    </header>
-
-    <main>
         <h2>Página de produtos</h2>
         <hr>
 
@@ -32,11 +13,5 @@
         </ul>
 
         <p>Todos os produtos com garantia!</p>
-    </main>
-
-    <footer>
-        <p>Site desenvolvido como exemplo</p>
-    </footer>
     
-</body>
-</html>
+<?php require "includes/rodape.php" ?>
