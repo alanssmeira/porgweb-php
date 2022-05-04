7dea8d0810126e44a0a565bbc8d1cc61e6b72c7e
alanssmeira <alan.sobreirasa@gmail.com>
2022-05-04 16:54:48 -0300

    Update rodape.php

--- site/includes/rodape.php
+++ site/includes/rodape.php
@@ -6,8 +6,18 @@
         $dataHora = new DateTime();
         $dataHora->setTimezone(new DateTimeZone('America/Sao_Paulo'));
         $DateAndTime = $dataHora->format("d-m-Y h:i:s a");
-        ?>
+    ?>
+
+    <!-- ** CORREÇÃO -->
+    <?php
+    date_default_timezone_set("America/Sao_Paulo");
+    $data = date("d/m/Y H:i")
+    ?>
+
     <p><?=$DateAndTime?></p>
+
+    <!-- ** CORREÇÃO -->
+    <p><?=$data?></p>
 
 </footer>
 
