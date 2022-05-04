ecfc49cd98926654af02f3efab1b933011c77e8c
alanssmeira <alan.sobreirasa@gmail.com>
2022-05-04 14:24:54 -0300

    Create textos.php

---
+++ textos.php
@@ -0,0 +1,5 @@
+<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse quo qui, repellendus labore ducimus eveniet quia sequi! Accusantium sit iure, optio at officia, eius voluptas magni voluptatem provident impedit tempora.</p>
+
+<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse quo qui, repellendus labore ducimus eveniet quia sequi! Accusantium sit iure, optio at officia, eius voluptas magni voluptatem provident impedit tempora.</p>
+
+<?php require "lista.html"; ?>
