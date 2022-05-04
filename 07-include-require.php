20e07f50228671c8b7750e3037544af5bbf8f6b7
alanssmeira <alan.sobreirasa@gmail.com>
2022-05-04 14:24:41 -0300

    Update 07-include-require.php

--- 07-include-require.php
+++ 07-include-require.php
@@ -1,31 +1,21 @@
+<?php include "recursos.php" ?>
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inclusão de Recursos</title>
 </head>
+
 <body>
     <h1>Inclusão de Recursos</h1>
     <hr>
 
-    <?php
-    const ESCOLA = "Senac";
-    $curso = "Programador Web";
-    $tecnologias = ["HTML", "CSS", "JavaScript", "PHP"];
 
-    function verificaIdade(int $valor):string {
-        if ($valor >= 18) {
-            return "maior";
-        } else {
-            return "menor";
-        }
-    }
-    ?>
-
-    <h2> <?=ESCOLA?></h2>
-    <p>Estamos no curso de <?=$curso?> </p>
+    <h2> <?= ESCOLA ?></h2>
+    <p>Estamos no curso de <?= $curso ?> </p>
 
     <ol>
 
@@ -35,11 +25,25 @@
 
     </ol>
 
-    <p>Chapolin Colorado tem 20 anos e é <b> <?=verificaIdade(20)?> </b> de idade.</p>
+    <p>Chapolin Colorado tem 20 anos e é <b> <?= verificaIdade(20) ?> </b> de idade.</p>
 
-    <p>Chapolin Colorado tem 16 anos e é <b> <?=verificaIdade(16)?> </b> de idade.</p>
+    <p>Chapolin Colorado tem 16 anos e é <b> <?= verificaIdade(16) ?> </b> de idade.</p>
 
-
+    <!-- include é incluir, se der erro o restante fica nnormal - require é requirir, essencial. Se der erro para tudo que vem depois -->
     
+    <article>
+        <h2>Conteúdo qualquer</h2>
+        <?php require "textos.php" ?>
+    </article>
+
+    <hr>
+
+    <article>
+        <h2>batatinha frita 1, 2, 3...</h2>
+        <?php include "textos.php" ?>
+    </article>
+
+
 </body>
+
 </html>
