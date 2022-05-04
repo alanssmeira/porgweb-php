ab54119df49bf61e74f869762c814a51878d1b4a
alanssmeira <alan.sobreirasa@gmail.com>
2022-05-04 14:50:35 -0300

    Create index.php

---
+++ site/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site XYZ</title>
+</head>
+<body>
+
+    <header>
+        <h1>Site XYZ</h1>
+
+        <nav>
+            <a href="index.php">Home</a>
+            <a href="produtos.php">Produtos</a>
+            <a href="servicos.php">Serviços</a>
+        </nav>
+    </header>
+
+    <main>
+        <h2>Bem-vindo ao site!</h2>
+        <hr>
+
+        <p>Esta é a página principal</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas consequatur laboriosam, natus vel excepturi dolor iure adipisci accusamus quia optio quis sint obcaecati tempora repudiandae explicabo sapiente! Eaque, harum vel.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem dolor consectetur magni facere maiores est cum, pariatur, quibusdam suscipit quaerat earum placeat voluptate? Delectus, labore! Quas omnis sint natus eligendi!</p>
+    </main>
+
+    <footer>
+        <p>Site desenvolvido como exemplo</p>
+    </footer>
+    
+</body>
+</html>
