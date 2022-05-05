932afd7dbb65142e31ad67cbfd7387fdde2fb9a3
alanssmeira <alan.sobreirasa@gmail.com>
2022-05-05 13:48:58 -0300

    titulo conforme a pag em php

--- site/includes/cabecalho.php
+++ site/includes/cabecalho.php
@@ -1,7 +1,12 @@
+<!-- Parte 1:
+$_SERVER['PHP_SELF'] -> traz os dados completos do endereço
+basename($_SERVER['PHP_SELF']) -> extrai apenas o nome.extensão -->
+
 <?php
 $pagina = basename($_SERVER['PHP_SELF']);
 echo $pagina;
 
+/* Parte 2: Condicional para avaliar qual página está aberta e definir qual titulo usar. */
 switch ($pagina) {
     case 'index.php': $titulo = "Página Inicial"; break;
     case 'produtos.php': $titulo = "Produtos"; break;
@@ -17,6 +22,8 @@ switch ($pagina) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Parte 3: Colocar a variável no title -->
     <title><?=$titulo?> - Site XYZ</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
